31528b5f571c319d996d0cb2e91a336fb06e2f13
suhjae <jwsuh25@kis.ac>
2024-04-01 14:38:33 +0900

    refacrored the updated classes

--- src/main/java/dev/suhjae/elgibility/Main.java
+++ src/main/java/dev/suhjae/elgibility/Main.java
@@ -1,24 +1,12 @@
 package dev.suhjae.elgibility;
 
-import java.util.ArrayList;
+import javax.swing.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        // Get all the students (arraylist)
-        ArrayList<Student> students = StudentManager.getStudentList();
-        ArrayList<Course> APCourses = dev.suhjae.elgibility.CourseManager.Main.getAllAP();
-
-        /**
-         for (Student student : students) {
-         for (Course course : APCourses) {
-         if (student.getGradeLevel() >= course.getMinGradeLevel()) {}
-         if (student.getGPA() >= course.getMinGpaCutOff()) {}
-         if (course.satReferenceBest()) { //Look for the best SAT score in the student's record
-         }
-         else { //Look for the latest SAT score in the student's record
-         }
-         }
-         }
-         /**/
+        SwingUtilities.invokeLater(() -> {
+            new MainFrame();
+        });
     }
 }
