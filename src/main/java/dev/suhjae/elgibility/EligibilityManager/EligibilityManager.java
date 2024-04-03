5d3dab7dd237b25ef01ad1f9526b61bbdc9d328e
suhjae <jwsuh25@kis.ac>
2024-04-03 14:17:00 +0900

    Drafted EligibilityManager Structure

--- src/main/java/dev/suhjae/elgibility/EligibilityManager/EligibilityManager.java
+++ src/main/java/dev/suhjae/elgibility/EligibilityManager/EligibilityManager.java
@@ -1,20 +1,35 @@
 package dev.suhjae.elgibility.EligibilityManager;
 
+import dev.suhjae.elgibility.CourseManager.Course;
 import dev.suhjae.elgibility.CourseManager.CourseManager;
+import dev.suhjae.elgibility.StudentManager.Student;
 import dev.suhjae.elgibility.StudentManager.StudentManager;
+
+import java.util.ArrayList;
 
 public class EligibilityManager {
     CourseManager courseManager;
     StudentManager studentManager;
+    ArrayList<Course> apCourses;
+    ArrayList<Student> students;
+
+    private boolean checkCourseElegibility(Student student, Course course) {
+        return true;
+    }
+
+    private ArrayList<Course> getEligibleCourses(Student student) {
+        return null;
+    }
 
     EligibilityManager(CourseManager cm, StudentManager sm) {
         this.courseManager = cm;
         this.studentManager = sm;
     }
 
+
     //Method that runs automatically
     public String[][] Go() {
-        return some2Darray;
+        return null;
     }
 
 
